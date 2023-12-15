770e50e788b25e48d92acc54ade87a9b31997bd5
Lchandrika <lakshmichandrika.marolix@gmail.com>
2023-12-15 17:16:37 +0530

    lucky

--- project4/xyz/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Employee
-
-class Employee_serializers(serializers.ModelSerializer):
-    class Mete:
-        model = Employee
-        fields = "__all__" 
--- project4/xyz/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import Employee_details
-
-url_patterns =[
-    path("emp/",Employee_details.as_view(),name = 'emp')
-]
-
-
